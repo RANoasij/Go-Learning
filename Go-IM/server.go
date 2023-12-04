1a5dc0329011166627092279191018cbe15d1050
RANoasij <civita129738@outlook.com>
2023-12-04 14:33:38 +0800

    v0.3 added a go func in Handler to Broadcast user message

--- Go-IM/server.go
+++ Go-IM/server.go
@@ -1,5 +1,3 @@
-// What you expect in the server.go?
-
 package main
 
 import (
@@ -11,101 +9,104 @@ import (
 type Server struct {
 	Ip   string
 	Port int
-	// 在线用户的列表
-	Onlinemap map[string]*User // map是一个key-value结构，key是string类型，value是*User类型
-	mapLock   sync.RWMutex     // 读写锁 (在sync里有很多锁的实现)
-	// 消息广播的channel
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
 	Message chan string
 }
 
-// N 大写，表示public，此方法对外可用
-func NewServer(ip string, port int) *Server { //* and &
-	server := &Server{ // 创建一个server对象，返回一个指针，指向Server的地址? 指向地址可以修改值，而不是拷贝值。
-		Ip:   ip,
-		Port: port,
-		// 初始化
-		Onlinemap: make(map[string]*User), // 初始化map，make初始化map，make初始化channel
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User), //这里存的是？
 		Message:   make(chan string),
 	}
+
 	return server
 }
 
-// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线user .
-func (s *Server) ListenMessager() {
+// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessager() {
 	for {
-		msg := <-s.Message // 监听管道中的数据，如果有数据，就读取出来，没有数据就阻塞
-		// 将msg发送给全部的在线user
-		s.mapLock.Lock()                  // 加锁 为啥要加锁呢？因为Onlinemap是线程不安全的，所以要加锁。
-		for _, cli := range s.Onlinemap { // cli value, user对象
-			cli.C <- msg // 将msg发送给User(cli)用户的管道发过去，随后User(cli)用户的ListenMessage()方法就会读取到msg。详见user.go
+		msg := <-this.Message
+
+		//将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
 		}
-		s.mapLock.Unlock() // 解锁
+		this.mapLock.Unlock()
 	}
 }
 
 // 广播消息的方法
-func (s *Server) BroadCast(user *User, msg string) {
-	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg // 组织消息
-	s.Message <- sendMsg                                     // 将消息发送到channel中
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
 }
 
-func (s *Server) Handler(conn net.Conn) { // conn is a socket 套接字作为形参
-	fmt.Println("连接建立成功")
-	// 发回给客户端
-	// 用户上线，将用户加入到onlinemap中
-	s.mapLock.Lock() // 加锁
-	user := NewUser(conn, s)
-	s.Onlinemap[user.Name] = user // 将用户加入到onlinemap中
-	s.mapLock.Unlock()            // 解锁
-	// 广播当前用户上线消息
-	s.BroadCast(user, "已上线")
-	// 接收客户端消息. 需要Read -> block of Socket. 用goroutine来处理，防止阻塞。go func(){}().
-	go func()  {	
-		buffer := make([]byte, 1700) // 如果大小写成宏，怎么写： const BUFFER_SIZE = 1024. buffer := make([]byte, BUFFER_SIZE)
+func (this *Server) Handler(conn net.Conn) {
+	//...当前链接的业务
+	//fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	//用户上线,将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+	// 接收客户端发送的消息，并广播
+	go func() {
+		defer conn.Close() // exit后关闭连接，避免CLOSE_WAIT。（就是服务器8888连客户端的这连接。）
+		buf := make([]byte, 6666)
 		for {
-			a, err := conn.Read(buffer) // 读取用户数据. a是读取到的字节数(长度)
-			if a == 0 { // 用户下线
-				s.BroadCast(user, "下线")
-				return
-			}
-			if err != nil && err != io.EOF { // io.EOF表示读取到文件末尾
+			n, err := conn.Read(buf)
+			if err != nil {
 				fmt.Println("conn.Read err:", err)
 				return
 			}
-			// 提取用户的消息(去除'\n')
-			msg := string(buffer[:a-1]) // 从buffer中取出用户输入的消息
-			
-		
-	}
-
-	// 当前handler阻塞
-	select {} // 阻塞当前handler
+			//将得到的消息进行广播
+			msg := string(buf[:n-1]) //去掉最后的\n
+			this.BroadCast(user, msg)
+		}
+	}()
+	//当前handler阻塞
+	select {}
 }
 
 // 启动服务器的接口
-func (s *Server) Start() {
-	// socket listen
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port)) // 监听协议udp/tcp, 定义一个格式化的字符串，s.Ip和s.Port分别替换%s和%d
-	if err != nil {                                                        // go语言中没有异常机制，使用error来原地处理错误
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	fmt.Println("Starting server at2 " + this.Ip + ":" + fmt.Sprintf("%d", this.Port) + "...")
+	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
-	// close listen socket
+	//close listen socket
 	defer listener.Close()
 
-	// 启动监听Message的goroutine
-	go s.ListenMessager()
+	//启动监听Message的goroutine
+	go this.ListenMessager()
 
-	// 在循环中。服务器，需要阻塞监听用户连接请求。如果有客户端连接，创建一个连接。
 	for {
-		// accept
-		conn, err := listener.Accept() // 接收连接 //basic socket programming
+		//accept
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("listener.Accept err:", err)
-			continue //如果出错，继续下一个循环
+			fmt.Println("listener accept err:", err)
+			continue
 		}
-		// do handler 业务回调处理
-		go s.Handler(conn)
 
+		//do handler
+		go this.Handler(conn)
 	}
 }
